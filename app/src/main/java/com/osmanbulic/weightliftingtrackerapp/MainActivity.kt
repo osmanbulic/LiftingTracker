a2bbf4c056f017ae357d842bb88394a9ee9f9430
Osman Jablan Bulic <osmanbulic@gmail.com>
2025-06-06 21:42:41 +0200

    Lesson 7 stuff - Screen UI with start and view history buttons

--- app/src/main/java/com/osmanbulic/weightliftingtrackerapp/MainActivity.kt
+++ app/src/main/java/com/osmanbulic/weightliftingtrackerapp/MainActivity.kt
@@ -3,27 +3,23 @@ package com.osmanbulic.weightliftingtrackerapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.osmanbulic.weightliftingtrackerapp.ui.theme.WeightLiftingTrackerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             WeightLiftingTrackerAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +27,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    WeightLiftingTrackerAppTheme {
-        Greeting("Android")
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = "Lifting Tracker",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(32.dp))
+        Button(onClick = { /* TODO */ }) {
+            Text("Start Workout")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /* TODO */ }) {
+            Text("View History")
+        }
     }
 }
